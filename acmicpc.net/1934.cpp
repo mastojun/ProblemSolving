bc0b5472ab1b4ea869156f567f24dae16a13b579
Mastojun <mastojun@gmail.com>
2013-08-07 00:58:38 +0900

    acmicpc.net easy

--- acmicpc.net/1934.cpp
+++ acmicpc.net/1934.cpp
@@ -1,9 +1,24 @@
 #include <stdio.h>
 
+int gcd(int a, int b)
+{
+	if(b == 0) return a;
+	return gcd(b, a%b);
+}
+
 int main()
 {
 	int N, a, b;
 	scanf("%d", &N);
+
+	while(N --> 0)
+	{
+		scanf("%d %d", &a, &b);
+
+		int g = gcd(a, b);
+
+		printf("%d\n", a*b/g);
+	}
 
 	return 0;
 }
