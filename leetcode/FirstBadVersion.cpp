6de85ef0e3bb29156d039c86ef1dd161d6e6c9d3
mastojun <mastojun@gmail.com>
2015-10-22 20:55:41 +0900

    leetcode First Bad Version
    간단한 이분법으로 푸는 문제.
    (l + r)이 over flow가 발생하지 않도록 중간값을 l + (r - l) / 2 로 구함.

---
+++ leetcode/FirstBadVersion.cpp
@@ -0,0 +1,24 @@
+/** simple bisection method problem.
+ */
+
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int l = 1;
+        int r = n;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (isBadVersion(m)) {
+               r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return l;
+    }
+};
+
