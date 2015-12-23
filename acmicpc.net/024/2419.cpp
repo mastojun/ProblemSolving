a80a4ed4db3a5d79523c22094599f70d41cd5f3d
mastojun <mastojun@gmail.com>
2015-12-23 23:42:47 +0900

    acmicpc 2419 WA
    http://www.csc.kth.se/contest/boi/beetle-spoiler.pdf
    
    를읽고 나름대로 생각해서 풀어보았으나 틀림 ㅠㅠ

---
+++ acmicpc.net/024/2419.cpp
@@ -0,0 +1,76 @@
+#include <cstdio>
+#include <algorithm>
+#include <climits>
+#include <cstring>
+
+using namespace std;
+
+int n, m;
+int candy[301];
+int leftDp[301][301];
+int rightDp[301][301];
+
+int right(int l, int r, int remain);
+
+int left(int l, int r, int remain) {
+	if (remain == 0) return 0;
+
+	int& result = leftDp[l][r];
+	if (result != -1) return result;
+	result = INT_MAX;
+	if (l > 0) {
+		result = remain * (candy[l] - candy[l - 1]) + left(l - 1, r, remain - 1);
+	}
+	if (r <= n) {
+		result = min(result, remain * (candy[r] - candy[l]) + right(l - 1, r, remain - 1));
+	}
+
+	return result;
+}
+
+int right(int l, int r, int remain) {
+	if (remain == 0) return 0;
+
+	int& result = rightDp[l][r];
+	if (result != -1) return result;
+	result = INT_MAX;
+	if (r < n) {
+		result = remain * (candy[r + 1] - candy[r]) + right(l, r + 1, remain - 1);
+	}
+	if (l >= 0) {
+		result = min(result, remain * (candy[r] - candy[l]) + left(l, r + 1, remain - 1));
+	}
+
+	return result;
+}
+
+int main() {
+	scanf("%d %d", &n, &m);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", candy + i);
+	}
+
+	candy[n] = 0;
+
+	sort(candy, candy + n + 1);
+
+	int zeroIndex = 0;
+	for (int i = 0; i < n; i++) {
+		if (candy[i] == 0) {
+			zeroIndex = i;
+			break;
+		}
+	}
+	int result = 0;
+	for (int i = 0; i <= n; i++) {
+		memset(leftDp, -1, sizeof(leftDp));
+		memset(rightDp, -1, sizeof(rightDp));
+		result = max(result, m * i - left(zeroIndex, zeroIndex + 1, i));
+	}
+	printf("%d\n", result);
+
+	return 0;
+}
+
+
