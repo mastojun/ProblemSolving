6a3781da9a3b1f83470475de841ea6d9f09f3e68
mastojun <mastojun@gmail.com>
2015-12-26 00:02:52 +0900

    acmicpc.net 2156 AC

--- acmicpc.net/021/2156.cpp
+++ acmicpc.net/021/2156.cpp
@@ -1,3 +1,6 @@
+/**
+ * f(i, l) = max(wine[i] + f(i + 1, l + 1) 단 l이 2미만), f(i + 1, 0));
+ */
 #include <cstdio>
 #include <cstring>
 #include <algorithm>
@@ -6,11 +9,13 @@ using namespace std;
 
 int n;
 int wine[10000];
+int dp[10000][3];
 
 int solve(int idx, int len) {
     if (idx >= n) return 0;
 
-    int result = 0;
+    int& result = dp[idx][len];
+    if (result != -1) return result;
 
     result = solve(idx + 1, 0);
     if (len < 2) result = max(result, wine[idx] + solve(idx + 1, len + 1));
@@ -26,6 +31,7 @@ int main() {
         scanf("%d", wine + i);
     }
 
+    memset(dp, -1, sizeof(dp));
     printf("%d\n", solve(0, 0));
 
     return 0;
