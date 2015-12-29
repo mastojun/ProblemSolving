ad4fbd62f98c77f620ec4f5a1e9c8334ee2708c9
mastojun <mastojun@gmail.com>
2015-12-29 16:36:49 +0900

    acmicpc.net 1648
    풀이 방식대로 점화식 수정

--- acmicpc.net/016/1648.cpp
+++ acmicpc.net/016/1648.cpp
@@ -4,8 +4,7 @@
  *  아무리 고민해도 안되서 검색해보니 http://slidehot.com/resources/baekjoon-online-judge-1648.471337/ 이걸 발견
  *  두번째 슬라이드의 go(N, S) = N번째 칸을 채울 것이고, N번째 칸부터 칸 M개의~ 설명읽고 아! 하여 품
  *
- *  solve(N, S) = N번째 칼을 채울 것이고, N - M 부터 N - 1 까지의 상태가 S일때의 경우의 수 로 품
- *  이전 상태가 없는 n == 0인 경우에 대한 예외처리 때문에 더 복잡한듯 (-_-;;)
+ *  풀이에 있는 방식대로 solve함수를 고쳤더니 더 깔끔해짐!
  *
  *  오래전 topcoder에서 벽에 무언가 배열하는 문제도 이런 식으로 푸는거였는데 풀이가 안떠오르다니 읃리ㅏ32러ㅣㅏ
  */
@@ -35,7 +34,7 @@ int append(int status, vector<int> blocks) {
 
 int solve(int n, int m, int status) {
   if (n == N) {
-    return status == (1 << M) -1;
+    return status == 0;
   }
   if (m == M) {
     return solve(n + 1, 0, status);
@@ -51,16 +50,13 @@ int solve(int n, int m, int status) {
 
   result = 0;
 
-  if (n == 0) {
-    result += solve(n, m + 2, append(status, {1, 1}));
-    result += solve(n, m + 1, append(status, {0}));
-  } else if (isSet(status, 0)) {
-    if (isSet(status, 1)) {
-      result += solve(n, m + 2, append(status, {1, 1}));
+  if (isSet(status, 0)) {
+    result = solve(n, m + 1, append(status, {0}));
+  } else {
+    result = solve(n, m + 1, append(status, {1}));
+    if (!isSet(status, 1)) {
+      result += solve(n, m + 2, append(status, {0, 0}));
     }
-    result += solve(n, m + 1, append(status, {0}));
-  } else if (!isSet(status, 0)) {
-    result += solve(n, m + 1, append(status, {1}));
   }
 
   return result %= 9901;
