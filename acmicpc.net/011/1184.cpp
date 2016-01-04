835ce02a5b7220177098aebb8ec28b7b04cbf7a5
mastojun <mastojun@gmail.com>
2016-01-05 00:13:09 +0900

    acmicpc.net 1184

--- acmicpc.net/011/1184.cpp
+++ acmicpc.net/011/1184.cpp
@@ -1,30 +1,19 @@
+/**
+ * 간신히 통과함 -_ㅠ...
+ *
+ * N*N 을 움직이면서, 이 점을 기준으로 십자가로 땅을 4등분함. 이 점은 2사분면의 우하단 점을 나타냄
+ * 각 영역은 부분합을 이용하여빠르게 계산하도록함..
+ *
+ * 제첨현황 보니 100ms 이하로 푸는데 이건 900ms 정도 -_ㅠ..
+ */
 #include <cstdio>
 #include <map>
+#include <cstring>
 
 using namespace std;
 
 int N;
 int profitMap[51][51];
-
-struct Point {
-  int x, y;
-
-  Point(int x, int y) {
-    this->x = x;
-    this->y = y;
-  }
-
-  bool operator==(const Point& other) const {
-    return other.x == x && other.y == y;
-  }
-
-  bool operator<(const Point& other) const {
-    if (x == other.x) return y < other.y;
-    return x < other.x;
-  }
-};
-
-map<int, map<Point, int>> profits[4];
 
 int main() {
   scanf("%d", &N);
@@ -37,25 +26,39 @@ int main() {
   }
 
   int result = 0;
-  for (int i = 1; i <= N; i++) {
-    int sum = 0;
-    for (int j = 1; j <= N; j++) {
-      for (int k = i; k <= N; k++) {
-        sum = 0;
-        for (int s = j; s <= N; s++) {
-          sum += profitMap[k][s] - profitMap[i - 1][s];
-
-          result += profits[0][sum][Point(k + 1, s + 1)]; // LEFT TOP
-          profits[0][sum][Point(i, j)]++;
-
-          result += profits[1][sum][Point(k + 1, j - 1)]; // RIGHT TOP
-          profits[1][sum][Point(i, s)]++;
-
-          result += profits[2][sum][Point(i - 1, s + 1)]; // LEFT BOTTOM
-          profits[2][sum][Point(k, j)]++;
-
-          result += profits[3][sum][Point(i - 1, j - 1)]; // RIGHT BOTTOM
-          profits[3][sum][Point(k, s)]++;
+  for (int i = 1; i < N; i++) {
+    for (int j = 1; j < N; j++) {
+      map<int, int> profits[2];
+      // left top
+      for (int k = i; k > 0; k--) {
+        int sum = 0;
+        for (int s = j; s > 0; s--) {
+          sum += profitMap[i][s] - profitMap[k - 1][s];
+          profits[0][sum]++;
+        }
+      }
+      // right top
+      for (int k = i; k > 0; k--) {
+        int sum = 0;
+        for (int s = j + 1; s <= N; s++) {
+          sum += profitMap[i][s] - profitMap[k - 1][s];
+          profits[1][sum]++;
+        }
+      }
+      // left bottom
+      for (int k = i + 1; k <= N; k++) {
+        int sum = 0;
+        for (int s = j; s > 0; s--) {
+          sum += profitMap[k][s] - profitMap[i][s];
+          result += profits[1][sum];
+        }
+      }
+      // right bottom
+      for (int k = i + 1; k <= N; k++) {
+        int sum = 0;
+        for (int s = j + 1; s <= N; s++) {
+          sum += profitMap[k][s] - profitMap[i][s];
+          result += profits[0][sum];
         }
       }
     }
